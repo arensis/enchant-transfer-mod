89dd083e3ef0aa31e2514be49d6ecd216cecae2f
Alfonso Madrid <arensis@gmail.com>
2020-12-25 15:34:22 +0100

    Refactored mod id string

--- src/main/java/net/alfonsormadrid/enchanttransfer/EnchantTransferMod.java
+++ src/main/java/net/alfonsormadrid/enchanttransfer/EnchantTransferMod.java
@@ -13,9 +13,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class EnchantTransferMod implements ModInitializer {
+	public static final String MOD_ID = "enchanttransfer";
+
 	public static final TransferTableBlock TRANSFER_TABLE_BLOCK = new TransferTableBlock();
 	public static final ItemGroup ENCHANT_TRANSFER = FabricItemGroupBuilder.build(
-			new Identifier("enchanttransfer", "general"),
+			new Identifier(MOD_ID, "general"),
 			() -> new ItemStack(TRANSFER_TABLE_BLOCK)
 	);
 	public static final TransferTableItem TRANSFER_TABLE_ITEM = new TransferTableItem(TRANSFER_TABLE_BLOCK);
@@ -25,9 +27,9 @@ public class EnchantTransferMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.BLOCK_ENTITY_TYPE, "modid:enchanttransfer", TRANSFER_TABLE_BLOCK_ENTITY);
-		Registry.register(Registry.ITEM, "enchanttransfer:magic_card_item", MAGIC_CARD_ITEM);
-		Registry.register(Registry.BLOCK,"enchanttransfer:transfer_table_block", TRANSFER_TABLE_BLOCK);
-		Registry.register(Registry.ITEM, "enchanttransfer:transfer_table_block", TRANSFER_TABLE_ITEM);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "magic_card_item"), MAGIC_CARD_ITEM);
+		Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "transfer_table_block"), TRANSFER_TABLE_BLOCK_ENTITY);
+		Registry.register(Registry.BLOCK,new Identifier(MOD_ID, "transfer_table_block"), TRANSFER_TABLE_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "transfer_table_block"), TRANSFER_TABLE_ITEM);
 	}
 }
